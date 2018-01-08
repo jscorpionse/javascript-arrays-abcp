7fea5f93244d0879ce72c8e693a2d6ae4ee50d6b
Jay Chen <jscorpionse@yahoo.com>
2018-01-08 21:01:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(){
+function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element)
 
 }
